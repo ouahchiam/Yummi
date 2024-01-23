ee4b589cd34f2ed7d13fa806a36274733de13666
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-01-23 12:49:53 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Ouahchia, Mohamed (JD) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
