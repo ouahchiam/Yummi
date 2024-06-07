21c0fc954ef58cae769efc566e4fba42dd59c078
Mohamed Ouahchia <Ouahchia.M@etoncollege.org.uk>
2024-06-07 11:19:53 +0100

    nah

---
+++ Yummi/Models/Recipe/AutocompleteResult.swift
@@ -0,0 +1,8 @@
+//
+//  AutocompleteResult.swift
+//  Yummi
+//
+//  Created by Ouahchia, Mohamed (JD) on 07/06/2024.
+//
+
+import Foundation
---
+++ Yummi/Models/Recipe/Recipe.swift
@@ -0,0 +1,8 @@
+//
+//  Recipe.swift
+//  Yummi
+//
+//  Created by Ouahchia, Mohamed (JD) on 07/06/2024.
+//
+
+import Foundation
